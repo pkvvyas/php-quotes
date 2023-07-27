352b73c916da9d1d1149a75a0fcaa5323128f7d5
pkvvyas <123542531+pkvvyas@users.noreply.github.com>
2023-07-27 14:25:28 +0530

    Create quote.sql

---
+++ quote.sql
@@ -0,0 +1,5 @@
+CREATE TABLE quote (id int, msg varchar(255));
+INSERT INTO quote VALUES (1, 'Love For All, Hatred For None');
+INSERT INTO quote VALUES (2, 'Change the world by being yourself');
+INSERT INTO quote VALUES (3, 'Every moment is a fresh beginning');
+INSERT INTO quote VALUES (4, 'Die with memories, not dreams');
